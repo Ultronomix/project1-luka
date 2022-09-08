b207e8eea2652a7e74db85473a3df429168b49dd
Luka Licheli <lukalicheli@gmail.com>
2022-09-08 13:11:42 -0700

    Added user class with getter and setter methods as well as getAllUsers method in UserDAO

---
+++ src/main/java/com/revature/project1/users/User.java
@@ -0,0 +1,69 @@
+package com.revature.project1.users;
+
+import java.util.Objects;
+
+public class User {
+
+    private int id;
+    private String username;
+    private int paycheck;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public int getPaycheck() {
+        return paycheck;
+    }
+
+    public void setPaycheck(int paycheck) {
+        this.paycheck = paycheck;
+    }
+
+    public User() {
+        super();
+    }
+
+    public User(int id, String username, int paycheck) {
+        this.id = id;
+        this.username = username;
+        this.paycheck = paycheck;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id && paycheck == user.paycheck && username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username, paycheck);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", paycheck=" + paycheck +
+                '}';
+    }
+}
--- src/main/java/com/revature/project1/users/UserDAO.java
+++ src/main/java/com/revature/project1/users/UserDAO.java
@@ -1,4 +1,48 @@
 package com.revature.project1.users;
 
+import com.revature.project1.common.datasource.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserDAO {
+
+    private final String select = "select * from app_users";
+
+    public List<User> getAllUsers() {
+
+        List<User> allUsers = new ArrayList<>();
+
+        try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+
+            // JDBC Statement objects are vulnerable to SQL injection
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(select);
+
+            allUsers = mapResultSet(rs);
+
+        } catch (SQLException e) {
+            System.err.println("Something went wrong when communicating with the database");
+            e.printStackTrace();
+        }
+        return allUsers;
+    }
+
+    private List<User> mapResultSet(ResultSet rs) throws SQLException {
+        List<User> users = new ArrayList<>();
+        while (rs.next()) {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPaycheck(rs.getInt("paycheck"));
+            users.add(user);
+        }
+        return users;
+    }
+
+
 }

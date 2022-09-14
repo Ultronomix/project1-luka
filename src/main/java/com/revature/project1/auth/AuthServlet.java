66b80054e95d67b344bd8dc1d2fe7f59edef51e5
Luka Licheli <lukalicheli@gmail.com>
2022-09-13 19:08:05 -0700

    Updated UserDAO to have method that finds by username and password, added a credentials class for logging in, and added a DataSourceException to handle exceptions

--- src/main/java/com/revature/project1/auth/AuthServlet.java
+++ src/main/java/com/revature/project1/auth/AuthServlet.java
@@ -1,8 +1,14 @@
 package com.revature.project1.auth;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.revature.project1.users.User;
 import com.revature.project1.users.UserDAO;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class AuthServlet extends HttpServlet {
 
@@ -12,4 +18,14 @@ public class AuthServlet extends HttpServlet {
         this.userDAO = userDAO;
     }
 
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper jsonMapper = new ObjectMapper();
+        resp.setContentType("application/json");
+        Credentials credentials = jsonMapper.readValue(req.getInputStream(), Credentials.class);
+        User loggedInUser = userDAO.findUserByUsernameAndPassword(credentials.getUsername(), credentials.getPassword())
+                .orElseThrow(() -> new RuntimeException("No user found with those credentials"));
+
+        resp.getWriter().write(jsonMapper.writeValueAsString(loggedInUser));
+    }
 }
--- src/main/java/com/revature/project1/users/UserDAO.java
+++ src/main/java/com/revature/project1/users/UserDAO.java
@@ -1,19 +1,21 @@
 package com.revature.project1.users;
 
-import com.revature.project1.common.datasource.ConnectionFactory;
+import com.revature.project1.common.exceptions.DataSourceException;
+import com.revature.project1.users.datasource.ConnectionFactory;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 public class UserDAO {
+    private final String baseSelect = "SELECT au.id, au.given_name, au.surname, au.email, au.username, au.role_id, ur.role " +
+            "FROM app_users au " +
+            "JOIN user_roles ur " +
+            "ON au.role_id = ur.id ";
 
     public List<User> getAllUsers() {
-        String select = "select * from users";
 
         List<User> allUsers = new ArrayList<>();
 
@@ -21,7 +23,7 @@ public class UserDAO {
 
             // JDBC Statement objects are vulnerable to SQL injection
             Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(select);
+            ResultSet rs = stmt.executeQuery(baseSelect);
 
             allUsers = mapResultSet(rs);
 
@@ -46,6 +48,26 @@ public class UserDAO {
             users.add(user);
         }
         return users;
+    }
+
+    public Optional<User> findUserByUsernameAndPassword(String username, String password) {
+
+        String sql = baseSelect + "WHERE au.username = ? AND au.password = ?";
+
+        try (Connection conn = ConnectionFactory.getInstance().getConnection()) {
+
+            // JDBC Statement objects are vulnerable to SQL injection
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, username);
+            pstmt.setString(2, password);
+            ResultSet rs = pstmt.executeQuery();
+            return mapResultSet(rs).stream().findFirst();
+
+        } catch (SQLException e) {
+            // TODO log this exception
+            throw new DataSourceException(e);
+        }
+
     }
 
 

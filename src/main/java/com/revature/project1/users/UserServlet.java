761973bbe875355b2e903f7acbebce022bb80199
Luka Licheli <lukalicheli@gmail.com>
2022-09-14 23:11:49 -0700

    Added Reimbursement.java

--- src/main/java/com/revature/project1/users/UserServlet.java
+++ src/main/java/com/revature/project1/users/UserServlet.java
@@ -3,9 +3,11 @@ package com.revature.project1.users;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.project1.common.ErrorResponse;
+import com.revature.project1.common.ResourceCreationResponse;
 import com.revature.project1.common.exceptions.DataSourceException;
 import com.revature.project1.common.exceptions.InvalidRequestException;
 import com.revature.project1.common.exceptions.ResourceNotFoundException;
+import com.revature.project1.common.exceptions.ResourcePersistenceException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -23,6 +25,7 @@ public class UserServlet extends HttpServlet {
     public UserServlet(UserService userService) {
         this.userService = userService;
     }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ObjectMapper jsonMapper = new ObjectMapper();
@@ -38,7 +41,7 @@ public class UserServlet extends HttpServlet {
         String idParam = req.getParameter("user_id");
 
         UserResponse requester = (UserResponse) userSession.getAttribute("authUser");
-        if (!requester.getRoleId().equals("0001") && !requester.getRoleId().equals("0002")){
+        if (!requester.getRoleId().equals("0001") && !requester.getRoleId().equals("0002")) {
             resp.setStatus(403);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(403, "Requester is not permitted to communicate with this endpoint")));
             return;
@@ -64,6 +67,33 @@ public class UserServlet extends HttpServlet {
 
 
         } catch (DataSourceException e) {
+            resp.setStatus(500);
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(500, e.getMessage())));
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper jsonMapper = new ObjectMapper();
+        resp.setContentType("application/json");
+        try {
+
+            NewUserRequest requestBody = jsonMapper.readValue(req.getInputStream(), NewUserRequest.class);
+            ResourceCreationResponse responseBody = userService.register(requestBody);
+            resp.getWriter().write(jsonMapper.writeValueAsString(responseBody));
+
+        } catch (InvalidRequestException | JsonMappingException e) {
+
+            resp.setStatus(400);
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(400, e.getMessage())));
+
+        } catch (ResourcePersistenceException e) {
+
+            resp.setStatus(409);
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(409, e.getMessage())));
+
+        } catch (DataSourceException e) {
+
             resp.setStatus(500);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(500, e.getMessage())));
         }

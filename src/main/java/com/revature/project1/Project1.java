75936611f8a62e083cff6a80a87cc402bd1414b1
Luka Licheli <lukalicheli@gmail.com>
2022-09-18 22:05:27 -0700

    Last bit of linting and formatting. Now on to adding log files and testing

--- src/main/java/com/revature/project1/Project1.java
+++ src/main/java/com/revature/project1/Project1.java
@@ -5,10 +5,10 @@ import com.revature.project1.auth.AuthServlet;
 import com.revature.project1.reimbursements.ReimbDAO;
 import com.revature.project1.reimbursements.ReimbService;
 import com.revature.project1.reimbursements.ReimbServlet;
-import com.revature.project1.users.User;
 import com.revature.project1.users.UserDAO;
 import com.revature.project1.users.UserService;
 import com.revature.project1.users.UserServlet;
+
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
@@ -23,9 +23,11 @@ public class Project1 {
 
         UserDAO userDAO = new UserDAO();
         ReimbDAO reimbDAO = new ReimbDAO();
+
         AuthService authService = new AuthService(userDAO);
         UserService userService = new UserService(userDAO);
         ReimbService reimbService = new ReimbService(reimbDAO);
+
         UserServlet userServlet = new UserServlet(userService);
         AuthServlet authServlet = new AuthServlet(authService);
         ReimbServlet reimbServlet = new ReimbServlet(reimbService);
@@ -35,7 +37,6 @@ public class Project1 {
         webServer.addServlet(rootContext, "UserServlet", userServlet).addMapping("/users");
         webServer.addServlet(rootContext, "AuthServlet", authServlet).addMapping("/auth");
         webServer.addServlet(rootContext, "ReimbursementServlet", reimbServlet ).addMapping("/reimb");
-
 
         webServer.start();
         webServer.getServer().await();

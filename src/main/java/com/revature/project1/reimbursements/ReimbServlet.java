79393157690562acc910ff49ae12160381da469c
Luka Licheli <lukalicheli@gmail.com>
2022-09-18 21:22:10 -0700

    Formatted reimbServlet.java

--- src/main/java/com/revature/project1/reimbursements/ReimbServlet.java
+++ src/main/java/com/revature/project1/reimbursements/ReimbServlet.java
@@ -5,11 +5,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.project1.common.ErrorResponse;
 import com.revature.project1.common.ResourceCreationResponse;
 import com.revature.project1.common.exceptions.*;
-import com.revature.project1.users.UserDAO;
-import com.revature.project1.reimbursements.ReimbDAO;
 import com.revature.project1.users.UserResponse;
-import com.revature.project1.reimbursements.ReimbResponse;
-import com.revature.project1.reimbursements.ReimbService;
+
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -17,13 +14,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class ReimbServlet extends HttpServlet {
-
     private final ReimbService reimbService;
-
     public ReimbServlet(ReimbService reimbService) {
         this.reimbService = reimbService;
     }
@@ -36,16 +30,15 @@ public class ReimbServlet extends HttpServlet {
         HttpSession userSession = req.getSession(false);
         if (userSession == null) {
             resp.setStatus(401);
-            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(401, "Requester not authenticated, please log in")));
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(401, "Not authenticated with the system, please log in.")));
             return;
         }
-
         String idParam = req.getParameter("reimbId");
-
         UserResponse requester = (UserResponse) userSession.getAttribute("authUser");
+
         if (!requester.getRoleId().equals("0003") && !requester.getRoleId().equals("0002")) {
             resp.setStatus(403);
-            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(403, "Requester is not permitted to communicate with this endpoint")));
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(403, "You do not have permission to communicate with this endpoint.")));
             return;
         }
 
@@ -57,64 +50,53 @@ public class ReimbServlet extends HttpServlet {
                 ReimbResponse reimbById = reimbService.getReimbById(idParam);
                 resp.getWriter().write(jsonMapper.writeValueAsString(reimbById));
             }
-
         } catch (InvalidRequestException | JsonMappingException e) {
             resp.setStatus(400);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(400, e.getMessage())));
-
-
         } catch (ResourceNotFoundException e) {
             resp.setStatus(404);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(400, e.getMessage())));
-
-
         } catch (DataSourceException e) {
             resp.setStatus(500);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(500, e.getMessage())));
         }
     }
-        @Override
-        protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            ObjectMapper jsonMapper = new ObjectMapper();
-            resp.setContentType("application/json");
+    @Override
+    protected void doPost (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper jsonMapper = new ObjectMapper();
+        resp.setContentType("application/json");
+        HttpSession reimbSession = req.getSession(false);
 
-            HttpSession reimbSession = req.getSession(false);
-
-            if (reimbSession == null) {
-                resp.setStatus(401);
-                resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(401, "Requester is not authenticated with the system, please log in.")));
-                return;
-            }
-            UserResponse requester = (UserResponse) reimbSession.getAttribute("authUser");
-
-            if (!requester.getRoleId().equals("0003") && !requester.getRoleId().equals("0002")) {
-                resp.setStatus(403);
-                resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(403, "Requester is not permitted to communicate with this endpoint.")));
-                return;
-            }
-
-            try {
-                NewReimbRequest requestBody = jsonMapper.readValue(req.getInputStream(), NewReimbRequest.class);
-                requestBody.setAuthorId(requester.getId());
-                ResourceCreationResponse responseBody = reimbService.newReimb(requestBody);
-                resp.getWriter().write(jsonMapper.writeValueAsString(responseBody));
-
-            } catch (InvalidRequestException | JsonMappingException e) {
-
-                resp.setStatus(400); // BAD REQUEST;
-                resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(400, e.getMessage())));
-            } catch (ResourcePersistenceException e) {
-
-                resp.setStatus(409); // CONFLICT; indicates that the provided resource could not be saved without conflicting with other data
-                resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(409, e.getMessage())));
-
-            } catch (DataSourceException e) {
-
-                resp.setStatus(500); // INTERNAL SERVER ERROR; general error indicating a problem with the server
-                resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(500, e.getMessage())));
-
-            }
+        if (reimbSession == null) {
+            resp.setStatus(401);
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(401, "You do not have permission to communicate with this endpoint.")));
+            return;
         }
+        UserResponse requester = (UserResponse) reimbSession.getAttribute("authUser");
+
+        if (!requester.getRoleId().equals("0003") && !requester.getRoleId().equals("0002")) {
+            resp.setStatus(403);
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(403, "You do not have permission to communicate with this endpoint.")));
+            return;
+        }
+        try {
+
+            NewReimbRequest requestBody = jsonMapper.readValue(req.getInputStream(), NewReimbRequest.class);
+            requestBody.setAuthorId(requester.getId());
+            ResourceCreationResponse responseBody = reimbService.newReimb(requestBody);
+            resp.getWriter().write(jsonMapper.writeValueAsString(responseBody));
+
+        } catch (InvalidRequestException | JsonMappingException e) {
+            resp.setStatus(400); // BAD REQUEST;
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(400, e.getMessage())));
+        } catch (ResourcePersistenceException e) {
+            resp.setStatus(409); // CONFLICT; indicates that the provided resource could not be saved without conflicting with other data
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(409, e.getMessage())));
+        } catch (DataSourceException e) {
+            resp.setStatus(500); // INTERNAL SERVER ERROR; general error indicating a problem with the server
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(500, e.getMessage())));
+        }
+    }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -124,35 +106,32 @@ public class ReimbServlet extends HttpServlet {
 
         if (reimbSession == null) {
             resp.setStatus(401);
-            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(401, "Requester is not authenticated with the system, please log in.")));
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(401, "Not authenticated with the system, please log in.")));
             return;
         }
+
         UserResponse requester = (UserResponse) reimbSession.getAttribute("authUser");
 
         if (!requester.getRoleId().equals("0002")) {
             resp.setStatus(403);
-            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(403, "Requester is not permitted to communicate with this endpoint.")));
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(403, "You do not have permission to communicate with this endpoint.")));
             return;
         }
-
         try {
+
             UpdateReimbRequest request = jsonMapper.readValue(req.getInputStream(), UpdateReimbRequest.class);
             reimbService.updateStatus(request);
+
         } catch (InvalidRequestException | JsonMappingException e){
             resp.setStatus(400);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(400, e.getMessage())));
-
         } catch (AuthenticationException e){
             resp.setStatus(400);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(400, e.getMessage())));
-
         } catch (DataSourceException e){
             resp.setStatus(500);
             resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(500, e.getMessage())));
         }
-
-
-
     }
 }
 
